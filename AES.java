f3bfed8ca6f1cb33a2c57d4d8a41f130d86dd4b8
Konrad <konrad_here@hotmail.com>
2016-10-11 04:05:53 +0000

    skeleton finished

--- AES.java
+++ AES.java
@@ -1,34 +1,66 @@
-// NOTE!!! TODO!! currently reads in 32 bytes but each digit is a hex digit so 
-// it should be 16 bytes per 32 hex digits. Same with input file.
-// Try reading file by line and then use the hexString -> bytes function
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import java.util.Arrays;
+import java.io.*;
 
 class Encrypter {
     private static byte[] sBox = javax.xml.bind.DatatypeConverter.parseHexBinary("637C777BF26B6FC53001672BFED7AB76CA82C97DFA5947F0ADD4A2AF9CA472C0B7FD9326363FF7CC34A5E5F171D8311504C723C31896059A071280E2EB27B27509832C1A1B6E5AA0523BD6B329E32F8453D100ED20FCB15B6ACBBE394A4C58CFD0EFAAFB434D338545F9027F503C9FA851A3408F929D38F5BCB6DA2110FFF3D2CD0C13EC5F974417C4A77E3D645D197360814FDC222A908846EEB814DE5E0BDBE0323A0A4906245CC2D3AC629195E479E7C8376D8DD54EA96C56F4EA657AAE08BA78252E1CA6B4C6E8DD741F4BBD8B8A703EB5664803F60E613557B986C11D9EE1F8981169D98E949B1E87E9CE5528DF8CA1890DBFE6426841992D0FB054BB16");
     private byte[] key;
-    private FileInputStream inFile;
+    private BufferedReader inFile;
     private FileOutputStream outFile;
 
 
     public Encrypter(byte[] key, String inFileName) throws FileNotFoundException {
         this.key = key;
-        this.inFile = new FileInputStream(inFileName);
+        this.inFile = new BufferedReader(new FileReader(inFileName));
         this.outFile = new FileOutputStream(inFileName + ".enc");
-        System.out.println(Arrays.toString(sBox));
     }
 
     public void encrypt() throws IOException {
-        byte[] buffer = new byte[32];
-        while(this.inFile.read(buffer) == 32) {
-            this.outFile.write(buffer);
-            this.inFile.read(); //read newline
+        
+        // TODO: KeyExpansions (separate 128 bit key for each round)
+        
+        
+        byte[] buffer;
+        for(String line = this.inFile.readLine(); line != null; line = this.inFile.readLine()) {
+            buffer = javax.xml.bind.DatatypeConverter.parseHexBinary(line);
+            if(buffer.length != 16) {
+                System.out.println("Input line is not 128 bits");
+                System.exit(1);
+            }
+            
+            for(int i = 0; i < 14; i++){
+                if(i == 0) {
+                    this.addRoundKey(buffer);
+                }
+                
+                if(i == 13) {
+                    // Final round (no mix columns):
+                    this.subBytes(buffer);
+                    this.shiftRows(buffer);
+                    this.addRoundKey(buffer);
+                } else {
+                    // non-final round:
+                    this.subBytes(buffer);
+                    this.shiftRows(buffer);
+                    this.mixColumns(buffer);
+                    this.addRoundKey(buffer);
+                }   
+            }
         }
+    }
+    
+    private void addRoundKey(byte[] input) {
+        System.out.println("addRoundKey unimplemented");
+    }
+    
+    private void subBytes(byte[] input) {
+        System.out.println("subBytes unimplemented");
+    }
+    
+    private void shiftRows(byte[] input) {
+        System.out.println("shiftRows unimplemented");
+    }
+    
+    private void mixColumns(byte[] input) {
+        System.out.println("mixColumns unimplemented");
     }
 }
 
@@ -43,15 +75,17 @@ class AES {
         String keyFileName = args[1];
         String inFileName = args[2];
 
-        byte[] key = new byte[64];
+        byte[] key = { 0 };
 
         try {
-            FileInputStream keyStream = new FileInputStream(keyFileName);
-            if(keyStream.read(key) != 64) {
-                System.out.println("Error reading key, not 256 bits");
+            BufferedReader keyReader = new BufferedReader(new FileReader(keyFileName));
+            String keyString = keyReader.readLine();
+            key = javax.xml.bind.DatatypeConverter.parseHexBinary(keyString);
+            
+            if(key.length != 32) {
+                System.out.println("Key not 256 bits");
                 System.exit(1);
             }
-            keyStream.close();            
         } catch(FileNotFoundException e) {
             System.out.println("Unable to open file");
             System.exit(1);
@@ -73,6 +107,9 @@ class AES {
             }
         } else if(option.equals("d")) {
 
+        } else {
+            System.out.println("option flag should be d|e");
+            System.exit(0);
         }
     }
 }
