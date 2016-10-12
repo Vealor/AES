306142bb3b4e56cb1a5635af1a999adce99497c3
Konrad <konrad_here@hotmail.com>
2016-10-12 07:23:33 +0000

    key expansion core

--- AES.java
+++ AES.java
@@ -1,7 +1,9 @@
 import java.io.*;
+import java.util.*;
 
 class Encrypter {
     private static byte[] sBox = javax.xml.bind.DatatypeConverter.parseHexBinary("637C777BF26B6FC53001672BFED7AB76CA82C97DFA5947F0ADD4A2AF9CA472C0B7FD9326363FF7CC34A5E5F171D8311504C723C31896059A071280E2EB27B27509832C1A1B6E5AA0523BD6B329E32F8453D100ED20FCB15B6ACBBE394A4C58CFD0EFAAFB434D338545F9027F503C9FA851A3408F929D38F5BCB6DA2110FFF3D2CD0C13EC5F974417C4A77E3D645D197360814FDC222A908846EEB814DE5E0BDBE0323A0A4906245CC2D3AC629195E479E7C8376D8DD54EA96C56F4EA657AAE08BA78252E1CA6B4C6E8DD741F4BBD8B8A703EB5664803F60E613557B986C11D9EE1F8981169D98E949B1E87E9CE5528DF8CA1890DBFE6426841992D0FB054BB16");
+    private static byte[] rcon = javax.xml.bind.DatatypeConverter.parseHexBinary("8D01020408102040801B366CD8AB4D9A2F5EBC63C697356AD4B37DFAEFC5913972E4D3BD61C29F254A943366CC831D3A74E8CB8D01020408102040801B366CD8AB4D9A2F5EBC63C697356AD4B37DFAEFC5913972E4D3BD61C29F254A943366CC831D3A74E8CB8D01020408102040801B366CD8AB4D9A2F5EBC63C697356AD4B37DFAEFC5913972E4D3BD61C29F254A943366CC831D3A74E8CB8D01020408102040801B366CD8AB4D9A2F5EBC63C697356AD4B37DFAEFC5913972E4D3BD61C29F254A943366CC831D3A74E8CB8D01020408102040801B366CD8AB4D9A2F5EBC63C697356AD4B37DFAEFC5913972E4D3BD61C29F254A943366CC831D3A74E8CB8D");
     private byte[] key;
     private BufferedReader inFile;
     private FileOutputStream outFile;
@@ -15,7 +17,9 @@ class Encrypter {
 
     public void encrypt() throws IOException {
         
-        // TODO: KeyExpansions (separate 128 bit key for each round)
+        List<Byte> expandedKey = new ArrayList<Byte>();
+        expandedKey.addAll(Arrays.asList(toByteArr(this.key)));
+        expandedKey.addAll(keyExpansion(this.key));
         
         
         byte[] buffer;
@@ -28,23 +32,67 @@ class Encrypter {
             
             for(int i = 0; i < 14; i++){
                 if(i == 0) {
-                    this.addRoundKey(buffer);
+                    // this.addRoundKey(buffer);
                 }
                 
                 if(i == 13) {
                     // Final round (no mix columns):
-                    this.subBytes(buffer);
-                    this.shiftRows(buffer);
-                    this.addRoundKey(buffer);
+                    // this.subBytes(buffer);
+                    // this.shiftRows(buffer);
+                    // this.addRoundKey(buffer);
                 } else {
                     // non-final round:
-                    this.subBytes(buffer);
-                    this.shiftRows(buffer);
-                    this.mixColumns(buffer);
-                    this.addRoundKey(buffer);
+                    // this.subBytes(buffer);
+                    // this.shiftRows(buffer);
+                    // this.mixColumns(buffer);
+                    // this.addRoundKey(buffer);
                 }   
             }
         }
+    }
+    
+    public static Byte[] toByteArr(byte[] array) {
+        if (array == null) {
+            return null;
+        } else if (array.length == 0) {
+            return new Byte[0];
+        }
+        final Byte[] result = new Byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = new Byte(array[i]);
+        }
+        return result;
+    }
+    
+    private void keyExpansionCore(byte[] prevKey, int i) {
+        int ndx;
+        
+        // rotate
+        byte temp = prevKey[0];
+        for(ndx = 0; ndx < 3; ndx ++) {
+            prevKey[ndx] = prevKey[ndx + 1];
+        }
+        prevKey[3] = temp;
+
+        for(int j = 0; j < 4; j++) {
+            prevKey[j] = this.sBox[prevKey[j] + 128];
+        }
+
+        prevKey[0] ^= this.rcon[i];
+    }
+    
+    // TODO: finish this method, only does core expansion currently.
+    private List<Byte> keyExpansion(byte[] initKey) {
+        List<Byte> roundKeys = new ArrayList<Byte>();
+        byte[] temp = Arrays.copyOfRange(initKey, initKey.length - 4, initKey.length);
+    
+        for(int i = 1; i < 61; i++) {
+            // generate 4 bytes of key
+            keyExpansionCore(temp, i);
+            roundKeys.addAll(Arrays.asList(toByteArr(temp)));
+        }
+        
+        return roundKeys;
     }
     
     private void addRoundKey(byte[] input) {
